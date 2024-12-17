f263e22436ad7b74377d7c8317979f7e9c114156
Alireza <alireza21heidari@gmail.com>
2024-12-18 00:21:04 +0330

    remove subsips

--- Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7bb2fdc3579eecb0c9278b7a3c6a6ccd918abc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+782f824a749ef43b7aca8ce1394b9791b27e0998")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
